5ab9a4250986a30d7be91a8e40e260475d3c8e51
Roman Bodnar <wizzardrince@gmail.com>
2024-03-20 08:40:33 +0100

    Add Review Statistic Model

--- TODO.md
+++ TODO.md
@@ -6,12 +6,15 @@ Project Description
 
 ### Todo
 
+- [ ] Add Review Statistics Client and Query  
+- [ ] Add Subjects Client and Query  
+- [ ] Add Reviews Client and Query  
+- [ ] Add Assignments Client and Query  
 - [ ] Add Spaced Repetition System models  
 - [ ] Set up logging: Add simple console logging  
 - [ ] Set up Refit client: Authorization and JsonSerialization  
 - [ ] Set up logging: Choose logging framework  
 - [ ] Implement Get All Reviews  
-- [ ] Add Review Statistics model  
 
 ### In Progress
 
@@ -20,4 +23,5 @@ Project Description
 
 - [x] Add Reviews models  
 - [x] Add Assignments models  
+- [x] Add Review Statistics model  
 
---
+++ WaniKani.Relearn/Model/Reviews/ReviewStatistic.cs
@@ -0,0 +1,69 @@
+﻿namespace WaniKani.Relearn;
+
+public class ReviewStatistic
+{
+    /// <summary>
+    /// Timestamp when the review statistic was created.
+    /// </summary>
+    public DateTime CreatedAt { get; }
+
+    /// <summary>
+    /// Indicates if the associated subject has been hidden, preventing it from appearing in lessons or reviews.
+    /// </summary>
+    public bool Hidden { get; }
+
+    /// <summary>
+    /// Total number of correct answers submitted for the meaning of the associated subject.
+    /// </summary>
+    public int MeaningCorrect { get; }
+
+    /// <summary>
+    /// The current, uninterrupted series of correct answers given for the meaning of the associated subject.
+    /// </summary>
+    public int MeaningCurrentStreak { get; }
+
+    /// <summary>
+    /// Total number of incorrect answers submitted for the meaning of the associated subject
+    /// </summary>
+    public int MeaningIncorrect { get; }
+
+    /// <summary>
+    /// The longest, uninterrupted series of correct answers ever given for the meaning of the associated subject.
+    /// </summary>
+    public int MeaningMaxStreak { get; }
+
+    /// <summary>
+    /// The overall correct answer rate by the user for the subject, including both meaning and reading.
+    /// </summary>
+    public int PercentageCorrect { get; }
+
+    /// <summary>
+    /// Total number of correct answers submitted for the reading of the associated subject.
+    /// </summary>
+    public int ReadingCorrect { get; }
+
+    /// <summary>
+    /// The current, uninterrupted series of correct answers given for the reading of the associated subject.
+    /// </summary>
+    public int ReadingCurrentStreak { get; }
+
+    /// <summary>
+    /// Total number of incorrect answers submitted for the reading of the associated subject.
+    /// </summary>
+    public int ReadingIncorrect { get; }
+
+    /// <summary>
+    /// The longest, uninterrupted series of correct answers ever given for the reading of the associated subject.
+    /// </summary>
+    public int ReadingMaxStreak { get; }
+    
+    /// <summary>
+    /// Unique identifier of the associated subject.
+    /// </summary>
+    public int SubjectId { get; }
+
+    /// <summary>
+    /// The type of the associated subject, one of: 'kana_vocabulary', 'kanji', 'radical', or 'vocabulary'.
+    /// </summary>
+    public string SubjectType { get; }
+}
