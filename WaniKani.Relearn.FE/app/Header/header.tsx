a343714d88a14b77037e07c2e2983fd0b27e695b
Roman Bodnar <wizzardrince@gmail.com>
2026-02-07 17:40:42 +0100

    Create NavigationBar component

--- WaniKani.Relearn.FE/app/Header/header.tsx
+++ WaniKani.Relearn.FE/app/Header/header.tsx
@@ -1,5 +1,6 @@
 import { Link, NavLink } from "react-router";
 import "./header.css";
+import NavigationBar from "~/components/NavigationBar";
 
 const Header = () => {
     return (
@@ -8,44 +9,8 @@ const Header = () => {
                 <span className="header-logo">
                     <Link to="/">WaniKani: Relearn</Link>
                 </span>
-
-                <nav className="navigation">
-                    <ul>
-                        <li>
-                            <NavLink 
-                                to="/assignments"
-                                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
-                            >
-                                Assignments
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink 
-                                to="/kanji?type=kanji"
-                                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
-                            >
-                                Kanji
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink 
-                                to="/vocabulary?type=vocabulary"
-                                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
-                            >
-                                Vocabulary
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink 
-                                to="/kana-vocabulary?type=kana-vocabulary"
-                                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
-                            >
-                                Kana vocabulary
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
+                <NavigationBar/>
+            </header>  
         </div>
     );
 };
---
+++ WaniKani.Relearn.FE/app/components/NavigationBar.tsx
@@ -0,0 +1,45 @@
+import { NavLink } from "react-router";
+import "./NavigationBar.css";
+
+const NavigationBar = () => {
+    return (
+        <nav className="navigation">
+            <ul>
+                <li>
+                    <NavLink
+                        to="/assignments"
+                        className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+                    >
+                        Assignments
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/kanji?type=kanji"
+                        className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+                    >
+                        Kanji
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/vocabulary?type=vocabulary"
+                        className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+                    >
+                        Vocabulary
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/kana-vocabulary?type=kana-vocabulary"
+                        className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+                    >
+                        Kana vocabulary
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavigationBar;
